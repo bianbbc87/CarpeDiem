c716ec0f81aeecb04be4594b46a6c664854c06d6
sohee <sohee5143@naver.com>
2024-01-02 23:24:46 +0900

    [Feat] #Map - 지도 정보 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import TestPage from "@/TestPage.jsx";
+import Place from "./pages/Place/Place.jsx";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TestPage />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/place" element={<Place />} />
       </Routes>
     </>
   );
