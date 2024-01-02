a375e9afa037dd6980c1aa1eab0fb153d84bea15
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-01-03 01:48:30 +0900

    [FEAT] #home - home 구현

--- src/pages/IT/IT.jsx
+++ src/pages/IT/IT.jsx
@@ -1,16 +1,27 @@
 import {RouletteWrapper, ITContainer, PersonImage, YellowButton} from "@/styles/styles.jsx";
 import "@/styles//IT.css";
+import {useLocation, useNavigate} from "react-router-dom";
 
 export default function IT() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const data = location.state || null;
+  console.log("data : ", data);
+
+  const handleVoice = () => {
+    navigate("/voice-recognition");
+  }
+
   return (
     <RouletteWrapper>
 <ITContainer>
     <PersonImage />
-    <h4>오늘의 알쓸신잡</h4>
+    <h4>{data[0].title}</h4>
     <div className="container">
-      {/* 알쓸신잡 데이터 추가 */}
+      {data[1].content}
     </div>
-    <YellowButton>carpe diem!</YellowButton>
+    <YellowButton onClick={handleVoice}>carpe diem!</YellowButton>
 </ITContainer>
     </RouletteWrapper>
   )
--- src/pages/Randing/Randing.jsx
+++ src/pages/Randing/Randing.jsx
@@ -1,7 +1,37 @@
 import {RouletteWrapper, ITContainer, LogoImage, StyledParagraph, BlackButton, YellowButtonR} from "@/styles/styles.jsx";
 import "@/styles//Randing.css";
+import {useNavigate} from "react-router-dom";
+import {apiClient} from "@/api/ApiClient";
 
 export default function Randing() {
+    const navigate = useNavigate();
+
+    // dummy data
+    const data = [
+        {title: "알쓸신잡 제목"},
+        {content: "알쓸신잡 내용"}
+    ]
+
+    // test code
+
+    const getTest = () => {
+        navigate("/IT", {state: data});
+    }
+
+  const getITData = async () => {
+    try {
+      const response = await apiClient.get('경로');
+
+      navigate("/IT", {state: response});
+    } catch(error) {
+      console.log("알쓸신잡 error", error);
+    }
+  }
+
+  const handleVoice = () => {
+    navigate("/voice-recognition");
+  }
+
   return (
     <RouletteWrapper>
 <ITContainer>
@@ -12,8 +42,8 @@ export default function Randing() {
         carpe diem!
       </StyledParagraph>
     </div>
-    <BlackButton>오늘의 알쓸신잡</BlackButton>
-    <YellowButtonR>carpe diem!</YellowButtonR>
+    <BlackButton onClick={getTest}>오늘의 알쓸신잡</BlackButton>
+    <YellowButtonR onClick={handleVoice}>carpe diem!</YellowButtonR>
 </ITContainer>
     </RouletteWrapper>
   )
